e2f0712e8e72ba5c8c40774f6869e6065556803c
pallavi <pallaviprabhusp@gmail.com>
2022-07-17 10:37:51 -0700

    import addition

--- one-way/src/main/java/com/service/oneway/config/Config.java
+++ one-way/src/main/java/com/service/oneway/config/Config.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
